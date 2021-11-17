1612b77877dd92eddfd749974c8553c150b7c2ac
Loksli17 <ami0504@mail.ru>
2021-11-17 18:03:46 +1000

    for university

--- dist/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    token: 'U2FsdGVkX1/VeXsd7k/1N9UulfOgnD7nhEbhOH3w3U5wX0GoyeJyJntSMcHPDtXv764OYD6Cl3S0yPLl4mLB4uYpNdKlCbxck75MbbVuuxc=',
-    prefix: 'Livsi ',
-};
